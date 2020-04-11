87c41d80535d2cf15fde1d604e88ff6dd1a5a351
Kipkorir <ipkipkorir@gmail.com>
2020-04-11 12:42:13 +0300

    Completed challenge 1

--- src/estimator.py
+++ src/estimator.py
@@ -1,2 +1,27 @@
 def estimator(data):
-  return data
+		
+	#Convert or check that 'timeToElapse' is in days
+	if data['periodType'] == 'days':
+		timeInDays = data['timeToElapse']
+	elif data['periodType'] == 'weeks':
+		timeInDays = data['timeToElapse'] * 7
+	elif data['periodType'] == 'months':
+		timeInDays = data['timeToElapse'] * 30
+
+
+	#Estimates
+	estimate = {
+		'impact' : {
+			'currentlyInfected' : data['reportedCases'] * 10,
+			'infectionsByRequestedTime' : (data['reportedCases'] * 10) * (2 ** (timeInDays // 3)),
+			
+		},
+
+		'severeImpact' : {
+				'currentlyInfected' : data['reportedCases'] * 50,
+				'infectionsByRequestedTime' :  (data['reportedCases'] * 50) * (2 ** (timeInDays // 3)),
+				
+		}
+
+	}
+	return estimate
