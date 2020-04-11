cd27046a11fa484975fa50f53dc1cf34ca35a57b
Kipkorir <ipkipkorir@gmail.com>
2020-04-11 14:47:35 +0300

    completed challenge 2 and modified challenge 1

--- src/estimator.py
+++ src/estimator.py
@@ -8,19 +8,34 @@ def estimator(data):
 	elif data['periodType'] == 'months':
 		timeInDays = data['timeToElapse'] * 30
 
+	#Compute estimates:
+	##Impact computation
+	impactCurrentlyInfected = data['reportedCases'] * 10
+	impactInfectionsByRequestedTime = (data['reportedCases'] * 10) * (2 ** (timeInDays // 3))
+	impactSevereCasesByRequestedTime = 0.15 * (data['reportedCases'] * 10) * (2 ** (timeInDays // 3))
+	impactHospitalBedsByRequestedTime = (0.35 * data['totalHospitalBeds']) - impactSevereCasesByRequestedTime
+
+	#Compute estimates:
+	##Severe Impact computation
+	severeImpactCurrentlyInfected = data['reportedCases'] * 50
+	severeImpactInfectionsByRequestedTime = (data['reportedCases'] * 50) * (2 ** (timeInDays // 3))
+	severeImpactSevereCasesByRequestedTime =  0.15 * (data['reportedCases'] * 50) * (2 ** (timeInDays // 3))
+	severeImpactHospitalBedsByRequestedTime = (0.35 * data['totalHospitalBeds']) - severeImpactSevereCasesByRequestedTime
 
 	#Estimates
 	estimate = {
 		'impact' : {
-			'currentlyInfected' : data['reportedCases'] * 10,
-			'infectionsByRequestedTime' : (data['reportedCases'] * 10) * (2 ** (timeInDays // 3)),
-			
+			'currentlyInfected' : impactCurrentlyInfected,
+			'infectionsByRequestedTime' : impactInfectionsByRequestedTime,
+			'severeCasesByRequestedTime' : impactSevereCasesByRequestedTime,
+			'HospitalBedsByRequestedTime' : impactHospitalBedsByRequestedTime
 		},
 
 		'severeImpact' : {
-				'currentlyInfected' : data['reportedCases'] * 50,
-				'infectionsByRequestedTime' :  (data['reportedCases'] * 50) * (2 ** (timeInDays // 3)),
-				
+				'currentlyInfected' : severeImpactCurrentlyInfected,
+				'infectionsByRequestedTime' :  severeImpactInfectionsByRequestedTime,
+				'severeCasesByRequestedTime' : severeImpactSevereCasesByRequestedTime,
+				'HospitalBedsByRequestedTime' : severeImpactHospitalBedsByRequestedTime
 		}
 
 	}
